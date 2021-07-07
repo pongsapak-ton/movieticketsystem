2d049679c24d6eefea96988498629398a29a0b70
ton <71214576+pongsapak-ton@users.noreply.github.com>
2021-07-07 23:11:56 +0700

    Add files via upload

--- select.py
+++ select.py
@@ -4,7 +4,10 @@ import tkinter as tk
 Font_tuple = ("Comic Sans MS", 20, "bold")
 Font1 = font=("Comic Sans MS", 15, "bold")
 
+button_identities = []
+
 def select_seat(screen):
+    button_identities = []
     global Font_tuple
     global Font1
     screen1 = Toplevel(screen)
@@ -14,7 +17,7 @@ def select_seat(screen):
     frame = Frame(screen1)
     frame.pack(side = TOP)
     seatframe = Frame(screen1,bg='#AB432D')
-    seatframe.pack(side = TOP,padx= 10 ,pady=20)
+    seatframe.pack(side = TOP,padx= 10 ,pady=30)
     entryframe = Frame(screen1, bg='blue')
     entryframe.pack(side = TOP,padx= 10 ,pady =10)
     label = Label(frame, bg='black', fg='white', text="Screen this side", width= 30 ,height = 2)
@@ -23,34 +26,64 @@ def select_seat(screen):
 
 
 
-    seat_1 = tk.Button(seatframe,text='A1',width = 10 ,height = 2,bg = 'yellow')
+    seat_1 = tk.Button(seatframe,text='A1',width = 10 ,height = 2,bg = 'yellow',command = lambda : onClickseat(seat_1))
     seat_1.grid(column=0,row= 0 ,padx= 5 ,pady =5)
-    seat_2 = tk.Button(seatframe,text = 'A2',width = 10, height = 2,bg='yellow')
+    button_identities.append(seat_1)
+
+    seat_2 = tk.Button(seatframe,text = 'A2',width = 10, height = 2,bg='yellow',command = lambda : onClickseat(seat_2))
     seat_2.grid(column=1 ,row=0 ,padx= 5 ,pady =5)
-    seat_3 = tk.Button(seatframe,text = 'A3',width = 10, height = 2 ,bg= 'yellow')
+    button_identities.append(seat_2)
+
+    seat_3 = tk.Button(seatframe,text = 'A3',width = 10, height = 2 ,bg= 'yellow',command = lambda : onClickseat(seat_3))
     seat_3.grid(row=0 ,column=2 ,padx= 5 ,pady =5)
-    seat_4 = tk.Button(seatframe, text='B1', width=10, height=2, bg='yellow')
+    button_identities.append(seat_3)
+
+    seat_4 = tk.Button(seatframe, text='B1', width=10, height=2, bg='yellow',command = lambda : onClickseat(seat_4))
     seat_4.grid(row=1 ,column=0 ,padx= 5 ,pady =5)
-    seat_5 = tk.Button(seatframe, text='B2', width=10, height=2, bg='yellow')
+    button_identities.append(seat_4)
+
+    seat_5 = tk.Button(seatframe, text='B2', width=10, height=2, bg='yellow',command = lambda : onClickseat(seat_5))
     seat_5.grid(row=1 ,column=1 ,padx= 5 ,pady =5)
-    seat_6 = tk.Button(seatframe, text='B3', width=10, height=2, bg='yellow')
+    button_identities.append(seat_5)
+
+    seat_6 = tk.Button(seatframe, text='B3', width=10, height=2, bg='yellow',command = lambda : onClickseat(seat_6))
     seat_6.grid(row=1 ,column=2 ,padx= 5 ,pady =5)
-    seat_7 = tk.Button(seatframe, text='C1', width=10, height=2, bg='yellow')
+    button_identities.append(seat_6)
+
+    seat_7 = tk.Button(seatframe, text='C1', width=10, height=2, bg='yellow',command = lambda : onClickseat(seat_7))
     seat_7.grid(row=2 ,column=0  ,padx= 5 ,pady =5)
-    seat_8 = tk.Button(seatframe, text='C2', width=10, height=2, bg='yellow')
+    button_identities.append(seat_7)
+
+    seat_8 = tk.Button(seatframe, text='C2', width=10, height=2, bg='yellow',command = lambda : onClickseat(seat_8))
     seat_8.grid(row=2 ,column= 1,padx= 5 ,pady =5)
-    seat_9 = tk.Button(seatframe, text='C3', width=10, height=2, bg='yellow')
+    button_identities.append(seat_8)
+
+    seat_9 = tk.Button(seatframe, text='C3', width=10, height=2, bg='yellow',command = lambda : onClickseat(seat_9))
     seat_9.grid(row=2 ,column=2 ,padx= 5 ,pady =5)
-    seat_10 = tk.Button(seatframe,text= 'A4' , width=10, height=2, bg='yellow')
+    button_identities.append(seat_9)
+
+    seat_10 = tk.Button(seatframe,text= 'A4' , width=10, height=2, bg='yellow',command = lambda : onClickseat(seat_10))
     seat_10.grid(row=0 ,column=3 ,padx= 5 ,pady =5)
-    seat_11 = tk.Button(seatframe,text= 'B4', width=10, height=2, bg='yellow')
+    button_identities.append(seat_10)
+
+    seat_11 = tk.Button(seatframe,text= 'B4', width=10, height=2, bg='yellow',command = lambda : onClickseat(seat_11))
     seat_11.grid(row=1 ,column= 3,padx= 5 ,pady =5)
-    seat_12 = tk.Button(seatframe,text = 'C4', width=10, height=2, bg='yellow')
+    button_identities.append(seat_11)
+
+    seat_12 = tk.Button(seatframe,text = 'C4', width=10, height=2, bg='yellow',command = lambda : onClickseat(seat_12))
     seat_12.grid(row=2 ,column=3 ,padx= 5 ,pady =5)
-    seat_13 = tk.Button(seatframe, text = 'D1', width = 25, height = 2 , bg = 'red')
+    button_identities.append(seat_12)
+
+    seat_13 = tk.Button(seatframe, text = 'D1', width = 25, height = 2 , bg = 'red',command = lambda : onClickseat(seat_13))
     seat_13.grid(row=3 ,column=0 ,columnspan=2  ,padx= 5 ,pady =5)
-    seat_14 = tk.Button(seatframe, text='D2', width=25, height=2, bg='red')
+    button_identities.append(seat_13)
+
+    seat_14 = tk.Button(seatframe, text='D2', width=25, height=2, bg='red',command = lambda : onClickseat(seat_14))
     seat_14.grid(column = 2 ,row =3,columnspan=2 ,padx= 5 ,pady =5)
+    button_identities.append(seat_14)
+
+    confirm = tk.Button(seatframe, text = 'confirm' ,width = 10,height =2 ,command = lambda :onClickcomfirm(button_identities))
+    confirm.grid(row = 4 ,column = 1,columnspan=2)
 
     Lastname = StringVar()
     Firstname = StringVar()
@@ -68,6 +101,39 @@ def select_seat(screen):
     phone_entry = Entry(entryframe,textvariable = phone,width = 20, borderwidth='4')
     phone_entry.grid(column=3, row=3,padx= 5 ,pady =5)
     phone_entry.config(borderwidth="2", relief=GROOVE, font=("Comic Sans MS", 15, "bold"))
+    ok = tk.Button(entryframe,text='ok')
+    ok.grid(column= 4 ,row = 1)
 
 
-    screen1.mainloop()
+def onClickseat(Button):
+    try:
+        Button.configure(state=tk.DISABLED)
+    except:
+        Button.configure(state="disabled")
+
+def get_select(row):
+    selected = []
+    for botton in row :
+        if (botton['state'] == 'disabled'):
+            print(botton, 'selected')
+            seatNum = botton.cget('text')
+            selected.append(seatNum)
+    return selected
+
+def onClickcomfirm(allseat):
+    selectedseat = get_select(allseat)
+    gold = []
+    silver = []
+    sofar = []
+    for Seat in selectedseat :
+        if(Seat[0] in ['A', 'B']):
+            print('Seat[0]:', Seat[0])
+            print('gold:', Seat)
+            gold.append(Seat)
+        elif (Seat[0] in ['C']):
+            print('Seat[0]:', Seat[0])
+            print('silver :', Seat)
+            silver.append(Seat)
+
+
+
